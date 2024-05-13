8b14383faffc19f1feb4aa2f40e98562137fedb3
AzrinHakimi88 <azrinhakimi630@gmail.com>
2024-05-13 19:19:09 +0800

    home title textsize

--- MyPortfolio/component/Home.jsx
+++ MyPortfolio/component/Home.jsx
@@ -6,11 +6,11 @@ const Home = () => {
     <div className='relative h-[90vh] w-[100vw] overflow-x-hidden bg-slate-950'>
         
         <div className='absolute inset-0 flex'>
-          <div className='transparent-bg h-full w-1/2 p-4'>
-            <h1 className='mt-8 text-white text-5xl poppins-bold text-center'>Hi, I am <span className='blue-gradient'>Azrin Hakimi bin Azizan</span> </h1>
+          <div className='transparent-bg h-full w-1/2  p-4'>
+            <h1 className='mt-8 text-white text-5xl poppins-bold text-center resposive-text-xl'>Hi, I am <span className='blue-gradient'>Azrin Hakimi bin Azizan</span> </h1>
           </div>
           <div className='h-full w-1/2 p-4 flex flex-col justify-center items-center '>
-            <h1 className='text-white text-6xl text-center mt-4 mb-2 green-gradient resposive-text'>Computer Science</h1>
+            <h1 className='text-white text-6xl text-center mt-4 mb-2 green-gradient resposive-text-md'>Computer Science</h1>
             <h3 className='text-white text-3xl text-center '>Information System </h3>
             <p className='text-white text-xl text-center'>in</p>
             <p className='text-white text-xl text-center'>University of Malaya</p>
