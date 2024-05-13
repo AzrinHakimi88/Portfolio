1f5c1ed63b0ffc5a053194b03841c5641017db05
AzrinHakimi88 <azrinhakimi630@gmail.com>
2024-05-13 19:24:59 +0800

    home text size

--- MyPortfolio/component/About.jsx
+++ MyPortfolio/component/About.jsx
@@ -6,7 +6,7 @@ const About = () => {
     <div className='h-[90vh] w-screen overflow-x-hidden bg-slate-800 px-24 overflow-hidden'>
       <div className='w-full h-full flex flex-col justify-center items-center md:p-24'> 
         <div className='w-full h-full flex flex-col xl:justify-between items-center xl:flex-row mt-[100%] xl:mt-0 md:mt-[20%] sm:mt-[80%]'>
-          <div className=''>
+          <div className='hidden sm:block'>
             <Parallax scale={[0.9,1.8]} tagOuter="figure">
               <img
                 src="./About.jpg"
@@ -18,7 +18,7 @@ const About = () => {
           </div>
           <div className=''>
             <Parallax speed={20} tagOuter="figure">
-              <div className='about-box p-8 max-w-[500px] rounded-md'>
+              <div className='about-box p-8 flex-1 rounded-md'>
                 <p className='text-white text-xl'>
                   I am Azrin Hakimi bin Azizan, student at University of Malaya. I study in Computer Science majoring in Information System. I am seeking for internship in web development. 
                 </p>
